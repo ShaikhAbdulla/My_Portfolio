21dabe9559ad19e3a00907a8a6a4e8c0bf0b5ea7
ShaikhAbdulla <abdullashaikh663@gmail.com>
2025-08-13 19:46:07 +0530

    latest commit 13-08-2025

--- src/App.js
+++ src/App.js
@@ -38,6 +38,10 @@ import {
   SiGoogleplay,
   SiAppstore
 } from "react-icons/si";
+import bandhanIcon from "../src/assets/icons/bandhan_mf.png"
+import LaIcon from "../src/assets/icons/LastingAsset-Logo.png"
+import ascelus from "../src/assets/icons/ascelus.webp"
+import zerowaste from "../src/assets/icons/zerowasteapp.jpg"
 
 const tools = [
   { name: "VS Code", icon: <SiVscodium /> },
@@ -116,28 +120,79 @@ export const skills = [
   { name: "Animations", desc: "Motion design in mobile UI" }
 ];
 
+// const experience = [
+//   {
+//     role: "Mobile Application Developer",
+//     company: "Applied Cloud Computing (ACC)",
+//     period: "Aug 2023 – Jan 2025",
+//     type: "Full-Time",
+//     description: [
+//       "Developed and maintained high-quality React Native applications for iOS and Android.",
+//       "Integrated Razorpay, Branch.io, and other third-party SDKs.",
+//       "Worked on apps like Bandhan Mutual Fund & ITecho Health, used by real clients.",
+//       "Collaborated with UI/UX teams and followed Agile practices."
+//     ]
+//   },
+//   {
+//     role: "React Native Intern",
+//     company: "Incretech Pvt Ltd (Youth Technology)",
+//     period: "Apr 2022 – Apr 2023",
+//     type: "Internship",
+//     description: [
+//       "Built internal tools like VCRM using React Native and Firebase.",
+//       "Learned mobile design patterns, navigation, and data persistence.",
+//       "Gained hands-on experience with version control and team workflows."
+//     ]
+//   }
+// ];
+
 const experience = [
+  {
+  role: "Mobile Application Developer",
+  company: "Velox Solutions pvt ltd",
+  period: "July 2025 – Present",
+  type: "Full-Time",
+  description: [
+    "Working on feature-rich React Native applications for iOS and Android, focusing on performance and scalability.",
+    "Collaborating with backend and design teams to deliver high-quality, user-centric mobile solutions.",
+    "Integrating third-party APIs and optimizing app workflows for better user experience."
+  ]
+},
+    {
+    role: "Freelance React Native Developer",
+    company: "Oberon Software Solutions Pvt Ltd",
+    period: "Mar 2025 – Jun 2025",
+    type: "Freelance",
+    description: [
+      "Independently developed 'OberonSS' – a smart billing and client management app tailored for the logistics industry.",
+      "Owned the complete development lifecycle: requirement analysis, architecture planning, UI/UX implementation, testing, and deployment.",
+      "Implemented offline-first data handling, multilingual support, and automated PDF invoice generation for operational efficiency.",
+      "Liaised directly with stakeholders to ensure feature alignment and delivered a fully functional, production-ready app within strict timelines."
+    ]
+  },
   {
     role: "Mobile Application Developer",
     company: "Applied Cloud Computing (ACC)",
     period: "Aug 2023 – Jan 2025",
     type: "Full-Time",
     description: [
-      "Developed and maintained high-quality React Native applications for iOS and Android.",
-      "Integrated Razorpay, Branch.io, and other third-party SDKs.",
-      "Worked on apps like Bandhan Mutual Fund & ITecho Health, used by real clients.",
-      "Collaborated with UI/UX teams and followed Agile practices."
+      "Designed, developed, and maintained high-quality React Native applications for iOS and Android platforms.",
+      "Integrated third-party SDKs including Razorpay for payments, Branch.io for deep linking, and other APIs to enhance app functionality.",
+      "Worked on production-grade apps such as Bandhan Mutual Fund and ITecho Health, serving real-world clients.",
+      "Collaborated closely with cross-functional teams including UI/UX designers and backend developers, following Agile methodologies."
     ]
   },
+
   {
     role: "React Native Intern",
     company: "Incretech Pvt Ltd (Youth Technology)",
     period: "Apr 2022 – Apr 2023",
     type: "Internship",
     description: [
-      "Built internal tools like VCRM using React Native and Firebase.",
-      "Learned mobile design patterns, navigation, and data persistence.",
-      "Gained hands-on experience with version control and team workflows."
+      "Contributed to the development of internal tools such as VCRM using React Native and Firebase.",
+      "Gained practical experience in mobile design patterns, navigation flows, and persistent data management.",
+      "Assisted in implementing new features, debugging issues, and improving app performance.",
+      "Collaborated with senior developers, learning industry best practices and version control workflows."
     ]
   }
 ];
@@ -145,30 +200,35 @@ const experience = [
 const projects = [
   {
     title: "Bandhan Mutual Fund Investor App",
-    description: "A finance app for retail investors with secure transaction flows and real-time performance tracking.",
+    icon:bandhanIcon,
+    description:"The official Bandhan Mutual Fund app helps you invest in mutual funds on the go. Whether you’re a first-time investor or planning long-term mutual fund investments for goals like retirement or tax saving, the app simplifies the journey.Cart Functionality: Save time by executing multiple mutual fund transactions in one go,  SIP Calculator: Plan your investments better using our built-in SIP calculator.",
     tech: ["React Native", "Jest"],
     apkLink: "https://play.google.com/store/apps/details?id=com.bandhanmf.inv",
   },
    {
     title: "Lasting Asset",
+    icon:LaIcon,
     description: "LastingAsset: Advanced Call Security for Your Peace of Mind In todays digital age, phone call fraud and impersonation scams are more prevalent than ever. LastingAsset is here to safeguard your communications with state-of-the-art technology that verifies the authenticity of every call you receive.",
     tech: ["React Native", "Native Bridging" , 'SDK integration'],
     apkLink: "https://play.google.com/store/apps/details?id=com.lastingasset.verify",
   },
    {
     title: "Zero Waste",
-    description: "Healthcare app integrated with hospital IT systems for long-term patient monitoring.",
+     icon:zerowaste,
+    description: "Waste Management Hub, a go-to platform for all things waste-related. Services include scrap collection, fostering a zero-waste society, efficient waste management, and providing clear segregation guidelines with the assistance of trained Safai Sathis. Additionally, it offer manpower solutions for proper waste management, as well as vehicle scrapping and dismantling services.",
     tech: ["React", "Firebase"],
     apkLink: "https://zerowastemngmnt.netlify.app/",
   },
   {
     title: "ITecho Health",
+     icon:ascelus,
     description: "Healthcare app integrated with hospital IT systems for long-term patient monitoring.",
     tech: ["React Native", "Firebase"],
     apkLink: "https://www.itechohealth.com/ascelus/",
   },
   {
     title: "Fitness At Your Doorsteps (F@D)",
+     icon:LaIcon,
     description: "A fitness marketplace app for gyms and personal trainers with login and dashboard modules.",
     tech: ["React Native", "Firebase"],
     apkLink: "#",
@@ -177,11 +237,23 @@ const projects = [
 
 const Card = ({ project }) => (
   <div className="card">
-    <h3 className="card-title">{project.title}</h3>
+  
+    {/* <div style={{flexDirection:'row'}}>
+    <img src="/assets/icons/bandhan_mf.webp" alt="Example" />
+    <h3 className="card-title">{project.title}</h3></div> */}
+      <div style={{ display: "flex", gap: "10px" }}>
+      <img
+        // src="/assets/icons/bandhan_mf.png"
+        src={project.icon}
+        // alt={project.title}
+        style={{ width: "40px", height: "40px", objectFit: "contain", borderRadius: "10px",justifyContent:'-moz-initial',alignSelf:'center' }}
+      />
+      <h3 className="card-title">{project.title}</h3>
+    </div>
     <p className="card-description">{project.description}</p>
     <p className="card-tech">Tech Stack: {project.tech.join(", ")}</p>
     <a className="card-link" href={project.apkLink} target="_blank" rel="noopener noreferrer">
-      Download APK
+      {project.title=='Zero Waste'?'Visit web':'Download APK'}
     </a>
   </div>
 );
@@ -298,7 +370,7 @@ export default function App() {
             <h3>
               {job.type === 'Internship' ? <FiUser className="icon" /> : <FiBriefcase className="icon" />} {job.role}
             </h3>
-            <span className="experience-type">{job.type}</span>
+            <span style={{width:'25%',alignSelf:'center'}} className="experience-type">{job.type}</span>
           </div>
           <p className="experience-company">
             <FiMapPin className="icon" /> {job.company}
